eae5715228ce0578502ccfb5d8224f1331287135
Andra Strachinaru <andra.popa1989@gmail.com>
2018-05-13 22:27:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,36 +33,6 @@ document.addEventListener('keypress', function(event) {
            } 
         });
 
-/*
-var ul = document.getElementById('ul');
-var input = document.getElementById('item');
-var btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(){
-    var currentInput = input.value;
-    input.value = '';
-    
-    var li = document.createElement('li');
-    var span = document.createElement('span');
-    var button = document.createElement('button');
-    
-    li.appendChild(span);
-    li.appendChild(button);
-    
-    span.textContent = currentInput;
-    button.textContent = 'Delete';
-    
-    ul.appendChild(li);
-    
-    button.onclick = function(e) {
-      ul.removeChild(li);  
-    };
-    
-    input.focus();
-});
-*/
-
-
 
 
 
